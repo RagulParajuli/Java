b2eb956c531ae6e0ef8c3b133ae8dd6ae8db27e4
Ragul <parajuliragul@gmail.com>
2023-05-12 23:11:30 +0530

    2nd

--- table_method.java
+++ table_method.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class table_method {
+    static int table(int x){
+        int m;
+        for(int i=1; i<=10; i++){
+            m = x*i;
+            System.out.println(x+" * "+i+" = "+m);
+        }
+        return 0;
+    }
     public static void main(String[] args) {
-        
+        Scanner sr = new Scanner(System.in);
+        System.out.println("Enter your number:");
+        int n = sr.nextInt();
+        int table = table(n);
+
     }
 }
