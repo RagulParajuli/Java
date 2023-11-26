ccc04785b09408537d3d9a91f58d3d624278e943
RAGUL <parajuliragul@gmail.com>
2023-11-27 00:24:56 +0530

    just rewatch

--- str_prbsol.java
+++ str_prbsol.java
@@ -7,26 +7,26 @@ public class str_prbsol {
     Scanner sr = new Scanner(System.in);
 
     // qns-01
-    // System.out.println("Enter your string:");
-    // String str = sr.nextLine();
-    // // String name = "Ragul";
-    // System.out.println(str.toLowerCase());
+    System.out.println("Enter your string:");
+    String str = sr.nextLine();
+    // String name = "Ragul";
+    System.out.println(str.toLowerCase());
 
-    // qns-02
-    // System.out.println("Enter your String:");
-    // String text = sr.nextLine();
-    // System.out.println(text.replace(" ","_"));
+    //qns-02
+    System.out.println("Enter your String:");
+    String text = sr.nextLine();
+    System.out.println(text.replace(" ","_"));
 
     // qns-03
-    // System.out.println("Enter your name:");
-    // String name = sr.nextLine();
-    // System.out.println("Dear,"+name+" thanks a lot");
+    System.out.println("Enter your name:");
+    String name = sr.nextLine();
+    System.out.println("Dear,"+name+" thanks a lot");
 
     // qns-04
-    // System.out.println("Enter your string:");
-    // String detect = sr.nextLine();
-    // System.out.println(detect.indexOf("  "));
-    // System.out.println(detect.indexOf("   "));
+    System.out.println("Enter your string:");
+    String detect = sr.nextLine();
+    System.out.println(detect.indexOf("  "));
+    System.out.println(detect.indexOf("   "));
     // if compiler gives -1 as ans then it means there is no double or triple space
 
     // qns-05
