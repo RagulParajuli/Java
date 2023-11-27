52a021635b487daecf380decb322ee28d14033d5
RAGUL <parajuliragul@gmail.com>
2023-11-27 18:51:48 +0530

    tool as package

---
+++ Packages/pack.java
@@ -0,0 +1,7 @@
+import tools.AdvCal;// import packageName.className
+import tools.Calc; // import packageName.className
+
+public class pack {
+    Calc c1 = new Calc();
+    AdvCal ac1 = new AdvCal(); 
+}
